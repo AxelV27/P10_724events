15ba5403a76b42ab87c195fa6935419d26400fed
Axel Verdasca <axel.verdasca@numericable.fr>
2024-04-18 12:07:16 +0200

    Filtre opérationnel

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -16,7 +16,8 @@ const EventList = () => {
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
+      // Ajout du filtre pour afficher uniquement les évènemetents dont le type correspond à celui sélectionné
+      : data?.events.filter((event)=> event.type === type)) || []
   ).filter((event, index) => {
     if (
       (currentPage - 1) * PER_PAGE <= index &&
